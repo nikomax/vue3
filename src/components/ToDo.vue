c43510a106a5f4bd4bdde4f7e3b3c7a811d82f5e
maks <maks@coderiver.com.ua>
2018-06-18 20:04:37 +0300

    first

---
+++ src/components/ToDo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="todo-app">
+    <div class="todo__input">
+      <input type="text" v-model="inputValue">
+      <p>{{inputValue}}</p>
+    </div>
+    <div class="todo__list">
+      <to-do-item v-for="item in list" :todo-text="item.title" :key="item.id" :todo-id="item.id" @delete="deleteTodo"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './ToDoItem'
+
+export default {
+  components: {ToDoItem},
+  name: 'ToDo',
+  data () {
+    return {
+      inputValue: '',
+      list: [
+        {
+          id: 1,
+          title: 'Купить пожрать'
+        },
+        {
+          id: 2,
+          title: 'Пожрать'
+        },
+        {
+          id: 3,
+          title: 'Лечь спать'
+        }]
+    }
+  },
+  methods: {
+    deleteTodo (id) {
+      console.log(id)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="todo-item" :id="todoId">
+    {{todoText}}
+    <button @click="deleteTodo">x</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toDoItem',
+  props: {
+    todoText: {
+      type: String,
+      required: true
+    },
+    todoId: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    deleteTodo () {
+      this.$emit('delete', this.todoId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
